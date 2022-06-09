2b938c0c0b95befb5ea5c6b4e35e32347764fea1
Sepidehmnt <sepidehmontazer@hotmail.com>
2022-06-10 00:49:24 +0430

    Delete a few extra lines of code

--- assets/main.js
+++ assets/main.js
@@ -30,32 +30,3 @@ function randomPhotos() {
   }
 }
 randomPhotos();
-
-// let items = document.getElementsByClassName("items")[0];
-// console.log(items);
-// let item = document.createElement("div");
-// let imgElement = document.createElement("img");
-// let colorOverlay = document.createElement("div");
-// let text = document.createElement("p");
-// let innerTxt = document.createTextNode("picture 1");
-
-// item.style.position = "relative";
-// colorOverlay.style.width = "100" + "%";
-// colorOverlay.style.height = "200" + "px";
-// colorOverlay.style.backgroundColor = "rgba(0, 0, 0, 0.6)";
-// colorOverlay.style.position = "absolute";
-// colorOverlay.style.top = "0";
-
-// text.style.position = "absolute";
-// text.style.bottom = "8" + "px";
-// text.style.left = "8" + "px";
-// text.style.color = "#faebd7";
-
-// text.appendChild(innerTxt);
-// item.appendChild(imgElement);
-// item.appendChild(colorOverlay);
-// item.appendChild(text);
-
-// imgElement.setAttribute("src", "https://picsum.photos/id/0/400/200");
-// console.log(imgElement.src);
-// items.appendChild(item);

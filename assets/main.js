3dd211c1bc5e590ab9ce41dea5f643ed91d98889
Sepidehmnt <sepidehmontazer@hotmail.com>
2022-06-10 00:40:25 +0430

    Create some random photos in container with a tag and filter on photo and do it with native js

---
+++ assets/main.js
@@ -0,0 +1,61 @@
+const range = 50;
+function randomPhotos() {
+  for (let i = 1; i <= range; i++) {
+    let items = document.getElementsByClassName("items")[0];
+    let item = document.createElement("div");
+    let imgElement = document.createElement("img");
+    let colorOverlay = document.createElement("div");
+    let text = document.createElement("p");
+    let innerTxt = document.createTextNode(`picture ${i}`);
+
+    item.style.position = "relative";
+    colorOverlay.style.width = "100" + "%";
+    colorOverlay.style.height = "200" + "px";
+    colorOverlay.style.backgroundColor = "rgba(0, 0, 0, 0.6)";
+    colorOverlay.style.position = "absolute";
+    colorOverlay.style.top = "0";
+
+    text.style.position = "absolute";
+    text.style.bottom = "8" + "px";
+    text.style.left = "8" + "px";
+    text.style.color = "#faebd7";
+
+    text.appendChild(innerTxt);
+    item.appendChild(imgElement);
+    item.appendChild(colorOverlay);
+    item.appendChild(text);
+
+    imgElement.setAttribute("src", `https://picsum.photos/id/${i}/400/200`);
+    items.appendChild(item);
+  }
+}
+randomPhotos();
+
+// let items = document.getElementsByClassName("items")[0];
+// console.log(items);
+// let item = document.createElement("div");
+// let imgElement = document.createElement("img");
+// let colorOverlay = document.createElement("div");
+// let text = document.createElement("p");
+// let innerTxt = document.createTextNode("picture 1");
+
+// item.style.position = "relative";
+// colorOverlay.style.width = "100" + "%";
+// colorOverlay.style.height = "200" + "px";
+// colorOverlay.style.backgroundColor = "rgba(0, 0, 0, 0.6)";
+// colorOverlay.style.position = "absolute";
+// colorOverlay.style.top = "0";
+
+// text.style.position = "absolute";
+// text.style.bottom = "8" + "px";
+// text.style.left = "8" + "px";
+// text.style.color = "#faebd7";
+
+// text.appendChild(innerTxt);
+// item.appendChild(imgElement);
+// item.appendChild(colorOverlay);
+// item.appendChild(text);
+
+// imgElement.setAttribute("src", "https://picsum.photos/id/0/400/200");
+// console.log(imgElement.src);
+// items.appendChild(item);
